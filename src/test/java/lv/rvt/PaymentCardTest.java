b8c95656788a6fd5aa4e4dda02959ea252558727
Anna Hrističa <88726120+22DP1AHris@users.noreply.github.com>
2024-01-24 16:24:13 +0000

    PaymentCard class + test for it

---
+++ src/test/java/lv/rvt/PaymentCardTest.java
@@ -0,0 +1,24 @@
+package lv.rvt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import rvt.PaymentCard;
+
+public class PaymentCardTest {
+    @Test
+    public void testPlusMethod(){
+        PaymentCard petesCard = new PaymentCard(10);
+        assertEquals(10.0, petesCard.balance());
+        
+        boolean wasSuccessful = petesCard.takeMoney(8);
+        assertEquals(true, wasSuccessful);
+        assertEquals(2.0, petesCard.balance());
+
+        wasSuccessful = petesCard.takeMoney(4);
+        assertEquals(false, wasSuccessful);
+        assertEquals(2.0, petesCard.balance());
+
+    }
+}
